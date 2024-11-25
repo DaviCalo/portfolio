66bf1b1a2eaf5acc72066e84571c6da1320f7325
Davi C. Pes. <davicalo2d@gmail.com>
2024-11-25 16:26:39 -0300

    add left componets

---
+++ src/app/components/social-links-component/social-links-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-social-links-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './social-links-component.component.html',
+  styleUrl: './social-links-component.component.scss'
+})
+export class SocialLinksComponent {
+
+}
